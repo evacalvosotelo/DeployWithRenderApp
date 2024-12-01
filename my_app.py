4aa35f26859f25130adf4830336eb8bb2a401b3b
evacalvosotelo <78585528+evacalvosotelo@users.noreply.github.com>
2024-12-01 20:03:27 +0100

    Cambios

--- my_app.py
+++ my_app.py
@@ -99,7 +99,7 @@ heatmap_fig = go.Figure(data=go.Heatmap(
 heatmap_fig.update_layout(title='Correlation Heatmap')
 
 app = dash.Dash(__name__)
-server = app.server
+# server = app.server
 
 app.layout = html.Div([
     dcc.Tabs([
@@ -462,6 +462,7 @@ def predict_grade(n_clicks, hours_studied, attendance, sleep_hours, motivation_l
 
     return ""
 
-
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    # Use the PORT environment variable for deployment
+    port = int(os.environ.get("PORT", 8050))  # Default to 8050 if PORT is not set
+    app.run_server(host="0.0.0.0", port=port)
